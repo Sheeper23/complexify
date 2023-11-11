e9200168f1d139f53d4e0168f50c7911dd19890a
Jai Misquith <jai.misquith@gmail.com>
2023-11-11 16:40:52 -0600

    canvas working but actually

--- frontend/src/hooks/useOnDraw.ts
+++ frontend/src/hooks/useOnDraw.ts
@@ -11,6 +11,9 @@ export function useOnDraw(onDraw: any) {
 
     function setCanvasRef(ref: any) {
         canvasRef.current = ref;
+        let rect = ref.parentNode.getBoundingClientRect();
+        ref.width = rect.width;
+        ref.height = rect.height;
     }
 
     function onCanvasMouseDown() {
@@ -70,7 +73,8 @@ export function useOnDraw(onDraw: any) {
 
     return {
         setCanvasRef,
-        onCanvasMouseDown
+        onCanvasMouseDown,
+        canvasRef
     }
 
 };
