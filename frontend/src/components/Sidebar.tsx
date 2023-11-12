0359e7da5306cce1f696edf6105805abe9b3378c
Jai Misquith <jai.misquith@gmail.com>
2023-11-11 21:11:54 -0600

    instructions added

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -26,8 +26,12 @@ export default function Sidebar({
     <div className="flex flex-col h-full gap-4 bg-neutral-700 w-[20%] items-center p-4">
         <Image src={mascot} alt="hedgehog" className="aspect-square w-full rounded-md" />
         <p className="text-[2.5vw] italic font-title text-transparent bg-clip-text scrolling-gradient-a">&nbsp;Complexify&nbsp;</p>
-        <div className="w-full scrolling-gradient-b border-2 border-white grow p-2 text-blue-400">
-          {gptMessage}
+        <div className="w-full bg-black border-2 border-white grow p-2 text-blue-400">
+          <p>{gptMessage}</p>
+          {(gptMessage != "" && gptMessage != "Processing your image...") && (<>
+            <p>&nbsp;</p>
+            <p>If I was right, click yes! If not, tell me what it is in the text box, then click no!</p>
+          </>)}
         </div>
         <div className="flex w-[80%] justify-between">
           <div onClick={onClickYes} className="bg-green-500 rounded-full py-2 px-4">
